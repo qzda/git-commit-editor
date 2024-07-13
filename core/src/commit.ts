8aba5bd9a06cbeb835427dddb779381e5d050743
qzda <qinzhidao@foxmail.com>
2024-07-13 17:08:53 +0800

    perf: parseCommit解析规则

--- core/src/commit.ts
+++ core/src/commit.ts
@@ -2,13 +2,16 @@ import type { Commit } from "../type"
 
 export function parseCommit(commitText: string): Commit {
   const lines = commitText.split("\n").filter((v) => !!v.trim())
+  const contextLines = lines.slice(3)
+  const blanKCount = contextLines[0].match(/^\s*/)?.[0].length || 0
+
   return {
     hash: lines[0].slice(7),
     author: lines[1].slice(8),
     date: lines[2].slice(8),
     context: lines
       .slice(3)
-      .map((v) => v.trim())
+      .map((v) => v.slice(blanKCount))
       .join("\n"),
   }
 }
--- index.ts
+++ index.ts
@@ -1,8 +1,15 @@
 import { CommitEditor } from "./core"
+import fullPaths from "./fullPaths"
 
-const repoVuejsCore = new CommitEditor("~/dev/other/vuejs/core")
+const repoVuejsCore = new CommitEditor(fullPaths["vuejs/core"])
 const commitHashList = await repoVuejsCore.getCommitHashHistory(5)
 commitHashList.forEach(async (hash) => {
   const { commit } = (await repoVuejsCore.getCommit(hash)) || {}
-  console.log(commit?.author)
+  console.log(commit)
 })
+
+// repoVuejsCore
+//   .getCommit("685e3f381c024b9f4023e60fe0545dc60d90d984")
+//   .then((res) => {
+//     console.log(res?.commit.context)
+//   })
